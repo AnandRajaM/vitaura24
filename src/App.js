c59dd5c128788e633f38d3426225bdf72b17a39d
aa <142321494+AnandRajaM@users.noreply.github.com>
2024-10-17 21:49:33 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-100 w-full h-svh absolute">
+       <div>
+          <h1 className="text-4xl text-center text-blue-500 relative bottom-0 left-0">Hello World</h1>
+       </div>
+    </div>
+  );
+}
+
+export default App;
